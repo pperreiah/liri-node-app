fddb155a78f6b460081c90b6183e5fa09f0d359d
pperreiah <31325721+pperreiah@users.noreply.github.com>
2017-12-19 22:21:21 -0500

    completed project

--- liri.js
+++ liri.js
@@ -1,39 +1,20 @@
 //liri.js
 
-//Require package request.  install with:  npm install request
-
+var fs = require("fs");
 var request = require('request');
-//  request('http://www.google.com', function(error, response, body) {
-//      console.log('error:', error); // Print the error if one occurred
-//      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//      console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-// Load the NPM Package inquirer
 var inquirer = require("inquirer");
-
-
 var Twitter = require('twitter');
-//Get twitter keys from keys.js
-var keys = require("./keys.js");
-var params = {screen_name: 'plptweeter'};
 
-//Require Spotify   install with:  npm install --save node-spotify-api
+var keys = require("./keys.js");
+var params = { screen_name: 'plptweeter' };
+
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
     id: "34e579868bff437fb6386e72cfb2e4e7",
     secret: "379ff493f0864ae29c8cd438f43cd0b5"
 });
 
-const imdb = require('imdb-search');
-var Omdb = require('omdb');
-const OmdbApi = require('omdb-api-pt')
- 
-// Create a new instance of the module.
-const omdb = new OmdbApi({
-  apiKey: "97578add", 
-  baseUrl: "https://omdbapi.com/"
-})
+var omdb = require('omdb');
 
 // Prompt for user choice of activity 
 inquirer
@@ -49,135 +30,141 @@ inquirer
     ])
     .then(function(inquirerResponse) {
 
-            if (inquirerResponse.userActions === "my-tweets") {
+        if (inquirerResponse.userActions === "my-tweets") {
 
-                var client = new Twitter(keys);
-                // Then run a request to twitter
-				client.get('statuses/user_timeline', params, function(error, tweets, response) {
-				  if (!error) {
-				  	if (tweets.length < 19) {
-				  		console.log("Here are all of your " + tweets.length + " tweets:")
-				  		for (i=0; i<tweets.length; i++) {
-				  			console.log(tweets[i].text + "  " + tweets[i].created_at);
-				  		}  //for
-				  	} else {
-				  		console.log("Here are all of your last 20 tweets:")
-				  		for (i=0; i<20; i++) {
-				  			console.log(tweets[i].text + "  " + tweets[i].created_at);
-				  		}  //for				  		
-				  	};  //if-else tweets.length
+            var client = new Twitter(keys);
+            // Then run a request to twitter
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+                if (!error) {
+                    if (tweets.length < 19) {
+                        console.log("Here are all of your " + tweets.length + " tweets:")
+                        for (i = 0; i < tweets.length; i++) {
+                            console.log(tweets[i].text + "  " + tweets[i].created_at);
+                        } //for
+                    } else {
+                        console.log("Here are all of your last 20 tweets:")
+                        for (i = 0; i < 20; i++) {
+                            console.log(tweets[i].text + "  " + tweets[i].created_at);
+                        } //for                     
+                    }; //if-else tweets.length
 
-				  } else {
-				  	console.log(error)
-				  };
+                } else {
+                    console.log(error)
+                };
 
-				});   //client.get function    
+            }); //client.get function    
 
-            } else if (inquirerResponse.userActions === "spotify-this-song") {
+        } else if (inquirerResponse.userActions === "spotify-this-song") {
 
-                inquirer
-                    .prompt([{
-                        type: "input",
-                        name: "songName",
-                        message: "Please enter a song title?"
-                    }])
-                    .then(function(inquirerResponse) {
+            inquirer
+                .prompt([{
+                    type: "input",
+                    name: "songName",
+                    message: "Please enter a song title?"
+                }])
+                .then(function(inquirerResponse) {
 
-                            // spotify.search( { type: 'track', query: inquirerResponse.songName }, function(err, data) {          
-                            //     if (err) {
-                            //         return console.log('Error occurred: ' + err);
-                            // 	}else {
-                            // 		// console.log(data)
-                            // 		for(i=0; i<5; i++) {
-		                          //   	var artists = data[i].artists;
-		                          //   	var songName = data[i].name;
-		                          //   	var previewSongURL = data[i].preview_url;
-		                          //   	var album = data[i].album;
+                    if (inquirerResponse.songName === "") {
+                        inquirerResponse.songName = '"The Sign"';
+                    } else {
+                        inquirerResponse.songName = '"' + inquirerResponse.songName + '"';
+                    };
 
-		                          //       console.log("Here is the info on the song '" + songName + ":'");
-		                          //       console.log("Artist(s): " + artists);
-		                          //       console.log("Song name: " + songName);
-		                          //       console.log("Preview song here: " + previewSongURL);
-		                          //       console.log("From the album: " + album);
-                            // 		} //for
-                            //     }; //if
-                            // });  //spotify search
+                    spotify.search({ type: 'track', query: inquirerResponse.songName }, function(err, data) {
 
-                    //second then
-                            spotify
-                              .search({ type: 'track', query: inquirerResponse.songName })
-                              .then(function(response) {
-                              	results = JSON.stringify(response)
-                                // console.log(response);
-                                console.log(results[1].album)
-                              })  //search
-                              .catch(function(err) {
-                                console.log(err);
-                              });  //catch
+                        if (err) {
+                            return console.log('Error occurred: ' + err);
+                        } else {
+                            console.log("Here is the info on the song " + inquirerResponse.songName + ":")
+                            for (i = 0; i < data.tracks.items.length; i++) {
+                                console.log("Song name: " + data.tracks.items[i].name);
+                                console.log("From the album(s): " + data.tracks.items[i].album.name);
+                                console.log("Artist(s): " + data.tracks.items[i].artists[0].name);
+                                console.log("Preview song here: " + data.tracks.items[i].href);
+                            } //for tracks
+                        }; //if else
 
-                    }); 
+                    }); //spotify search & function
 
+                }); //prompt then
 
-                        } else if (inquirerResponse.userActions === "movie-this") {
+        } else if (inquirerResponse.userActions === "movie-this") {
 
-                            // Prompt for user movie input
-                            inquirer
-                                .prompt([{
-                                    type: "input",
-                                    name: "movieName",
-                                    message: "What movie are you interested in?"
-                                }])
-                                .then(function(inquirerResponse) {
+            // Prompt for user movie input
+            inquirer
+                .prompt([{
+                    type: "input",
+                    name: "movieName",
+                    message: "What movie are you interested in?"
+                }])
+                .then(function(inquirerResponse) {
 
-                                    // Search for a Movie, result is an array of movies 
-									imdb.search(inquirerResponse.movieName)
-									  .then(result => {
-									  	console.log(result)
-									    for (let movie of result) {
-									      // console.log(`${movie.id}: ${movie.title} - ${movie.year}`);
-									      console.log(movie.title, movie.Year, movie.Rated)
-									    }
-									  })
-									  .catch(err => {
-									    console.log(err);
-									  });
+                    if (inquirerResponse.movieName === "") {
+                        inquirerResponse.movieName = "Mr Nobody"
+                    };
 
+                    var movieSearchStrg = "http://www.omdbapi.com/?t=" + inquirerResponse.movieName + "&y=&plot=short&apikey=trilogy"
+                    // Then run a request to the OMDB API with the movie specified
+                    request(movieSearchStrg, function(error, response, body) {
+
+                        // If the request is successful (i.e. if the response status code is 200)
+                        if (!error && response.statusCode === 200) {
+                            console.log("Title of the movie: " + JSON.parse(body).Title)
+                            console.log("Release year: " + JSON.parse(body).Year)
+                            console.log("The IMDB Rating is: " + JSON.parse(body).Rated);
+                            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value)
+                            console.log("Country where movie produced: " + JSON.parse(body).Country)
+                            console.log("Language of the movie: " + JSON.parse(body).Language)
+                            console.log("Plot of the movie: " + JSON.parse(body).Plot)
+                            console.log("Actors in the movie: " + JSON.parse(body).Actors)
+                        }; //if
+
+                    }); // request
+                }); //then function
 
 
-                                    // // Then run a request to the OMDB API with the movie specified
-									// omdb.get({ title: inquirerResponse.name }, true, function(err, movie) {
-									//     // if(err) {
-									//     //     return console.error(err);
-									//     // }
-									 
-									//     if(!movie) {
-									//     	omdb.get({ title: "Mr Nobody" }, true, function(err, movie) {
-									//         	console.log("Movie title: " + movie.title);
-									// 		    console.log("Release year: " + formatYear(movie.year));
-									// 		    console.log("IMdB Rating: " + movie.Rated);
-									// 		    console.log("Rotten Tomatoes Rating: " + movie.tomatoRating);
-									// 		    console.log("Country of production: " + formatList(movie.Country));
-									// 		    console.log("Language: " + movie.Language);
-									// 		    console.log("Plot: " + movie.Plot);
-									// 		    console.log("Actors: " + formatList(movie.Actors));
-									// 		});  //omdb.get Mr Nobody
-									//     } else {
+        } else if (inquirerResponse.userActions === "do-what-it-says") {
 
-									//     console.log("Movie title: " + movie.title);
-									//     console.log("Release year: " + formatYear(movie.year));
-									//     console.log("IMdB Rating: " + movie.Rated);
-									//     console.log("Rotten Tomatoes Rating: " + movie.tomatoRating);
-									//     console.log("Country of production: " + formatList(movie.Country));
-									//     console.log("Language: " + movie.Language);
-									//     console.log("Plot: " + movie.Plot);
-									//     console.log("Actors: " + formatList(movie.Actors));
-									// 	} //else
-									// });  //omdb.get
+            fs.readFile("./random.txt", "utf8", function(error, data) {
 
-                                });  //then inquirer response
+                // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                    return console.log(error);
+                }
+                console.log(data);
 
-                        } else if (inquirerResponse.userActions === "do-what-it-says") {
-                            console.log("do-what-it-says = " + inquirerResponse.userActions);
-                        }; //if choice
+                var dataArr = data.split(",");
+                var songFind = dataArr[1];
 
-    }); //then function
+                spotify.search({ type: 'track', query: songFind }, function(err, data) {
+
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    } else {
+                        console.log("Here is the info on the song " + songFind + ":")
+                        for (i = 0; i < data.tracks.items.length; i++) {
+                            console.log("Song name: " + data.tracks.items[i].name);
+                            console.log("From the album(s): " + data.tracks.items[i].album.name);
+                            console.log("Artist(s): " + data.tracks.items[i].artists[0].name);
+                            console.log("Preview song here: " + data.tracks.items[i].href);
+                        } //for tracks
+                        for (i = 0; i < data.tracks.items.length; i++) {
+                            fs.appendFile("./log.txt",
+                                "Song name: " + data.tracks.items[i].name + "," +
+                                "From the album(s): " + data.tracks.items[i].album.name + "," +
+                                "Artist(s): " + data.tracks.items[i].artists[0].name + "," +
+                                "Preview song here: " + data.tracks.items[i].href,
+                                function(err) {
+                                    if (err) {
+                                        console.log(err);
+                                    };
+                                }); //fs.appendFile
+                        }; //for tracks
+                    }; //if else
+
+                }); //spotify search & function
+
+            }); //fs readFile
+        }; //if choice
+
+    }); //prompt then function
